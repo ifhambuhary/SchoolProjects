4055c9bcad4c1cf0bf5d0608c33317a7844f5618
Ifham Buhary <ifham.buhary@gmail.com>
2019-09-19 12:08:14 -0400

    {Lab 6 - MDI Text Editor}

---
+++ vb.net/Lab6MDITextEditor/Lab6_MDI_Text_Editor_Ifham/Lab6_MDI_Text_Editor_Ifham/TextForm.Designer.vb
@@ -0,0 +1,55 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmText
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.txtBox = New System.Windows.Forms.TextBox()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.SuspendLayout()
+        '
+        'txtBox
+        '
+        Me.txtBox.Location = New System.Drawing.Point(10, 3)
+        Me.txtBox.Multiline = True
+        Me.txtBox.Name = "txtBox"
+        Me.txtBox.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.txtBox.Size = New System.Drawing.Size(779, 440)
+        Me.txtBox.TabIndex = 0
+        '
+        'frmText
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txtBox)
+        Me.Name = "frmText"
+        Me.Text = "Text"
+        Me.ToolTip1.SetToolTip(Me, "Enter Text")
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtBox As TextBox
+    Friend WithEvents ToolTip1 As ToolTip
+End Class
